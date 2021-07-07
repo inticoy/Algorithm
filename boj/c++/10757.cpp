4a0e84aabcfc06b0186b23c0914e3f1359cd39fd
inticoy <inticoy0406@gmail.com>
2021-07-08 00:35:00 +0900

    using c++

---
+++ boj/c++/10757.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+  string str1, str2;
+  std::cin >> str1 >> str2;
+  std::cout << str1 << str2 << std::endl;
+
+  return 0;
+}
