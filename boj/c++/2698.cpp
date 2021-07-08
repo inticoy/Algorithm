e9502448bb7dac3ba415eea688112230f85cbc70
inticoy <inticoy0406@gmail.com>
2021-07-09 02:25:13 +0900

    unsolved 2698, 2775

---
+++ boj/c++/2698.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int P, n, k;
+  int i;
+  cin >> P;
+  for(i = 0; i < P; i++)
+  {
+    cin >> n >> k;
+
+
+
+  }
+  return 0;
+}
--- boj/c++/2775.cpp
+++ boj/c++/2775.cpp
@@ -3,7 +3,24 @@
 using namespace std;
 int main(void)
 {
+  int T, k, n, i; // variables for problem
+  int people, x, y; // variables for adding
+  cin >> T;
 
+  for (i = 0; i < T; i++) {
+    cin >> k;
+    cin >> n;
+    people = 0;
+    y = k + 1;
+
+    for (x = 1; x <= n; x++){
+      // people += x * y;
+      // y--;
+      // cout << "people: " << people << endl;
+    }
+
+    cout << people << endl;
+  }
 
   return 0;
 }
