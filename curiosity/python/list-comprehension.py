271e4f7cb6f46ac078ff9dcb575af01f898eb7b7
Inticoy <inticoy0406@gmail.com>
2021-09-09 00:32:09 +0900

    week 1

---
+++ curiosity/python/list-comprehension.py
@@ -0,0 +1,54 @@
+import time
+
+num = 10 ** 4
+
+start = time.time()
+list_comprehension = [i for i in range(num)]
+end = time.time()
+print("[for i in] :\t", (end - start))
+
+start = time.time()
+list_append = []
+for i in range(num):
+    list_append.append(i)
+end = time.time()
+print("[].append() :\t", (end - start))
+
+
+start = time.time()
+list_add = []
+for i in range(num):
+    list_add += [i]
+end = time.time()
+print("[] + [] :\t", (end - start))
+
+start = time.time()
+list_initialize = [0] * num
+for i in range(num):
+    list_initialize[i] = i
+end = time.time()
+print("[0,... 0] :\t", (end - start))
+
+start = time.time()
+list_range = list(range(num))
+end = time.time()
+print("list(range()) :\t", end - start)
+
+start = time.time()
+list_comprehension = [i**i for i in range(num)]
+end = time.time()
+print("[for i in] :\t", (end - start))
+
+start = time.time()
+list_append = []
+for i in range(num):
+    list_append.append(i**i)
+end = time.time()
+print("[].append() :\t", (end - start))
+
+
+start = time.time()
+for i in range(num):
+    pass
+end = time.time()
+print(end - start)
