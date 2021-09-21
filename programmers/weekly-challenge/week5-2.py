e72a486f755cd530a3f99604a5da4ced84c03e15
Inticoy <inticoy0406@gmail.com>
2021-09-21 13:20:36 +0900

    week5 improve

---
+++ programmers/weekly-challenge/week5-2.py
@@ -0,0 +1,11 @@
+def solution(word):
+    n = len(word)
+    word2num = 0
+    answer = 0
+
+     l = [781, 156, 31, 6, 1]
+      d = {'A': 0, 'E': 1, 'I': 2, 'O': 3, 'U': 4}
+       for i, j in enumerate(word):
+            answer += l[i] * d[j] + 1
+
+        return answer
