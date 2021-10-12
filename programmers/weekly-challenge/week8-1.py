ec22d7bf2d11746896fab20620dc8c580292d50f
Inticoy <inticoy0406@gmail.com>
2021-10-12 22:39:00 +0900

    unsolved week 8

---
+++ programmers/weekly-challenge/week8-1.py
@@ -0,0 +1,11 @@
+def solution(sizes):
+    answer = 0
+
+    list1, list2 = zip(*sizes)
+
+    return list1
+
+
+print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
