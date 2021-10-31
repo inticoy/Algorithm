424cec968b0aa24e1c13fddada8a00a385942f23
inticoy <inticoy0406@gmail.com>
2021-10-31 20:11:40 +0900

    implementing binary search

--- data-structure/c/ch1/BinarySearch.c
+++ data-structure/c/ch1/BinarySearch.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
+int BinarySearch(int ar[], int len, int target)
+{
+  int start = 0;
+  int end = len - 1;
+  int middle;
+
+  while(start <= end)
+  {
+    
+  }
 
 
-
-
-
-
-
-
+}
 
 int main()
 {
+  int arr[] = {1, 3, 5, 7, 9};
+  int idx;
+
+  idx = BinarySearch(arr, sizeof(arr)/sizeof(int), 4);
+  if(idx == -1)
+    printf("Search Failed\n");
+  else
+    printf("Target Index: %d \n", idx);
+
+  idx = BinarySearch(arr, sizeof(arr)/sizeof(int), 7);
+  if(idx == -1)
+    printf("Search Failed\n");
+  else
+    printf("Target Index: %d \n", idx);
 
 
   return 0;
