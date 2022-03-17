88f4bf4a7f12b71caeebd03a690240884f47f93e
inticoy <inticoy0406@gmail.com>
2022-03-17 14:42:11 +0900

    solved 1929

---
+++ boj/c++/1929.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+  int m, n, i, j;
+
+  cin >> m >> n;
+  bool is_prime[n + 1];
+  int prime_nums[n / 2 + 1];
+  int num_prime_nums = 0;
+
+  is_prime[0] = false;
+  is_prime[1] = false;
+
+  for (i = 2; i < n + 1; i++){
+    is_prime[i] = true;
+  }
+
+  for (i = 2; i <= n; i++) {
+    if (is_prime[i]){
+      prime_nums[num_prime_nums] = i;
+      num_prime_nums++;
+      for (j = 2 * i; j <= n; j += i){
+        is_prime[j] = false;
+      }
+    }
+  }
+
+  for (i = m; i <= n; i++){
+    if (is_prime[i]) {
+      cout << i << "\n";
+    }
+  }
+
+  return 0;
+}
