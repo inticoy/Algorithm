3b9fcf957b2e14a62826f26132d6e8078d060548
inticoy <inticoy0406@gmail.com>
2022-03-20 14:55:31 +0900

    gcd problem

--- algorithm/gcd-and-lcm/gcd.cpp
+++ algorithm/gcd-and-lcm/gcd.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int GCD_remain(int a, int b);
 int GCD_sub(int a, int b);
 int main(void) {
-  int a = 32;
-  int b = 64;
-  cout << GCD_remain(a, b) << endl;
+  int a = 5;
+  int b = 5;
   cout << GCD_sub(a, b) << endl;
   return 0;
 }
 
+// a == b case must be checked
 int GCD_remain(int a, int b) {
   if (a)
     return (a > b) ? GCD_remain(a % b, b) : GCD_remain(b, a);
