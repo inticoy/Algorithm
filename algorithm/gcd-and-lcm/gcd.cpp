24bb1fd2f303976784fb8fc65785ba00458b8d2d
inticoy <inticoy0406@gmail.com>
2022-03-20 16:24:51 +0900

    gcd

--- algorithm/gcd-and-lcm/gcd.cpp
+++ algorithm/gcd-and-lcm/gcd.cpp
@@ -2,26 +2,16 @@
 
 using namespace std;
 
-int GCD_remain(int a, int b);
-int GCD_sub(int a, int b);
+int GCD(int a, int b);
 int main(void) {
-  int a = 5;
-  int b = 5;
-  cout << GCD_sub(a, b) << endl;
+  int a = 468;
+  int b = 9120;
+  cout << GCD(a, b) << endl;
   return 0;
 }
 
-// a == b case must be checked
-int GCD_remain(int a, int b) {
-  if (a)
-    return (a > b) ? GCD_remain(a % b, b) : GCD_remain(b, a);
-  else
+int GCD(int a, int b) {
+  if (a == 0)
     return b;
-}
-
-int GCD_sub(int a, int b) {
-  if (a == b)
-    return a;
-  else
-    return a > b ? GCD_sub(a - b, b) : GCD_sub(b, a);
+  return a <= b ? GCD(b % a, a) : GCD(b, a);
 }
