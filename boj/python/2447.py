64382812a7e0d7c4994179aeebee369ed46c5110
inticoy <inticoy0406@gmail.com>
2022-04-15 18:16:03 +0900

    solved 2447 in python

--- boj/python/2447.py
+++ boj/python/2447.py
@@ -1,14 +1,24 @@
-def star(N):
-    if N == 3:
-        print("***\n* *\n***")
+def star(num:int, blank:bool):
+    num = int(num)
+    if blank:
+        return [[' ' for i in range(num)] for j in range(num)]
+    if num == 3:
+        return [['*', '*', '*'], ['*', ' ', '*'], ['*', '*', '*']]
     else:
-        for i in range(3):
-            star(N / 3)
-        star(N / 3)
-        print('   \n   \n   ')
-        star(N / 3)
-        for i in range(3):
-            star(N / 3)
+        lst = []
+        frame = star(num/3, False)
+        center = star(num/3, True)
+        for i in range(num):
+            if int(i / (num/3)) == 1:
+                lst.append(frame[int(i % (num/3))] + center[int(i % (num/3))] + frame[int(i % (num/3))])
+            else :
+                lst.append(frame[int(i % (num/3))] * 3)
+        return lst
 
 
-star(9)
+n = int(input())
+get_star = star(n, False)
+for i in get_star:
+    for j in i:
+        print(j, end="")
+    print()
