791edc2ba2e5e7c49f899178126089d3f985d7ad
inticoy <inticoy0406@gmail.com>
2022-04-16 13:07:43 +0900

    solved 2581

---
+++ boj/c++/2581.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int main(void) {
+  int m, n, i, j;
+  int sum = 0;
+  int min = INT_MAX;
+
+  cin >> m >> n;
+  bool is_prime[n + 1];
+  int prime_nums[n / 2 + 1];
+  int num_prime_nums = 0;
+
+  is_prime[0] = false;
+  is_prime[1] = false;
+
+  for (i = 2; i < n + 1; i++) {
+    is_prime[i] = true;
+  }
+
+  for (i = 2; i <= n; i++) {
+    if (is_prime[i]) {
+      prime_nums[num_prime_nums] = i;
+      num_prime_nums++;
+      for (j = 2 * i; j <= n; j += i) {
+        is_prime[j] = false;
+      }
+    }
+  }
+
+  for (i = m; i <= n; i++) {
+    if (is_prime[i]) {
+      if (min > i)
+        min = i;
+      sum += i;
+    }
+  }
+
+  if (sum == 0)
+    cout << "-1";
+  else {
+    cout << sum << "\n" << min;
+  }
+
+  return 0;
+}
