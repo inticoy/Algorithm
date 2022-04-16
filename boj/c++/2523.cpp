d57499933ab9e0db37484da771228affdd241c91
inticoy <inticoy0406@gmail.com>
2022-04-16 22:27:19 +0900

    solved 2523

---
+++ boj/c++/2523.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int n, i, j;
+  cin >> n;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < i + 1; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+
+  for (i = n - 2; i >= 0; i--) {
+    for (j = 0; j < i + 1; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+  return 0;
+}
