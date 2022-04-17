38c6d465d7d1c1f7a3c71dc4e207d3f995b8fb2c
inticoy <inticoy0406@gmail.com>
2022-04-17 15:30:19 +0900

    solved 10992

---
+++ boj/c++/10992.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int n, i, j;
+  cin >> n;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      cout << " ";
+    }
+    cout << "*";
+
+    if (i == n - 1) {
+      for (j = 0; j < 2 * i; j++) {
+        cout << "*";
+      }
+    } else if (i != 0) {
+      for (j = 0; j < 2 * i - 1; j++) {
+        cout << " ";
+      }
+      cout << "*";
+    }
+
+    cout << "\n";
+  }
+
+  return 0;
+}
