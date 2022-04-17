5c8b6071bbc912d6f7c1e8efe722be28b47101bb
inticoy <inticoy0406@gmail.com>
2022-04-17 14:22:10 +0900

    solved 10991

---
+++ boj/c++/10991.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int n, i, j;
+  cin >> n;
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      cout << " ";
+    }
+
+    for (j = 0; j < i + 1; j++) {
+      cout << "* ";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
