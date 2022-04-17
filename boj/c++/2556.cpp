483d884a551689aac1e8bbbcd62f321303076ff9
inticoy <inticoy0406@gmail.com>
2022-04-17 17:31:25 +0900

    solved 2556

---
+++ boj/c++/2556.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+  int n, i, j;
+  cin >> n;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      cout << "*";
+    }
+    cout << "\n";
+  }
+  return 0;
+}
