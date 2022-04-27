62c0b2df9545386ccb97cd6349f36fd4b13a62e2
Inticoy <inticoy0406@gmail.com>
2022-04-27 19:33:14 +0900

    solved 16394

---
+++ boj/c++/16394.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+  using namespace std;
+  ios::sync_with_stdio(false);
+
+  int n;
+  cin >> n;
+
+  cout << n - 1946;
+  return 0;
+}
