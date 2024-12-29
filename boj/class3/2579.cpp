e3f59a51e599f18c854ff483aa6d006dc4c43e87
inticoy <inticoy0406@gmail.com>
2024-12-29 14:43:10 +0900

    pass: 2579

--- boj/class3/2579.cpp
+++ boj/class3/2579.cpp
@@ -1,44 +1,33 @@
+#include <algorithm>
 #include <iostream>
 
 using namespace std;
-
-int maxRoute(int N, int *p, int currLv)
-{
-    if (currLv == 0)
-        return 0;
-    else if (currLv < 0)
-        return -1;
-    else if (currLv == 1)
-        return p[currLv];
-    else if (currLv == 2)
-        return p[currLv] + p[currLv - 1];
-
-    int two = maxRoute(N, p, currLv - 2);
-    int three = maxRoute(N, p, currLv - 3);
-
-    if (three < 0 || two > three + p[currLv - 1])
-    {
-        return two + p[currLv];
-    }
-    else
-    {
-        return three + p[currLv - 1] + p[currLv];
-    }
-}
 
 int main(int argc, char const *argv[])
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    int N;
-    int P[301];
+    int n;
+    int p[301];
+    int total[301];
 
-    cin >> N;
-    for (int i = 1; i <= N; i++)
-        cin >> P[i];
+    cin >> n;
 
-    cout << maxRoute(N, P, N);
+    for (int i = 1; i <= n; i++)
+        cin >> p[i];
+
+    for (int i = 0; i <= 300; i++)
+        total[i] = -1;
+
+    total[0] = 0;
+    total[1] = p[1];
+    total[2] = p[1] + p[2];
+
+    for (int i = 3; i <= n; i++)
+        total[i] = max(total[i - 3] + p[i - 1] + p[i], total[i - 2] + p[i]);
+
+    cout << total[n];
 
     return 0;
 }
