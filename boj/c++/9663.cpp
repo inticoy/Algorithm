3f599d3273283e12278c310ac9016b4bc0c5e8f6
inticoy <inticoy0406@gmail.com>
2024-12-31 18:33:25 +0900

    pass: 9663

--- boj/c++/9663.cpp
+++ boj/c++/9663.cpp
@@ -1,96 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
 
-void solved(int *baord, int solved, int *count, int number);
+using namespace std;
 
-int is_valid_row(int *board, int number) {
-  int *check;
-  int i;
+int nqueen(int curr, int n, int board[][16])
+{
+    int answer = 0;
+    int attack = 0;
 
-  check = (int *)malloc(sizeof(int) * number);
-  i = 0;
-  while (i < number)
-    check[i++] = 0;
-  i = 0;
-  while (i < number) {
-    if (board[i] != -1)
-      check[board[i]]++;
-    i++;
-  }
-  i = 0;
-  while (i < number) {
-    if (check[i] > 1) {
-      free(check);
-      return (0);
-    }
-    i++;
-  }
-  free(check);
-  return (1);
+    if (curr > n)
+        return 1;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (board[0][i])
+            continue;
+
+        attack = 0;
+
+        for (int j = 1; j < curr; j++)
+        {
+            if (board[j][0] == 0)
+                continue;
+
+            if (board[j][0] + j == i + curr || board[j][0] - j == i - curr)
+                attack = 1;
+        }
+
+        if (attack)
+            continue;
+
+        board[curr][0] = i;
+        board[0][i] = curr;
+
+        answer += nqueen(curr + 1, n, board);
+
+        board[curr][0] = 0;
+        board[0][i] = 0;
+    };
+    board[curr][0] = 0;
+
+    return answer;
 }
 
-int is_valid_diagonal(int *board, int number) {
-  int i;
-  int j;
+int main(int argc, char const *argv[])
+{
+    ios_base::sync_with_stdio(false); // use cpp buffer only
+    cin.tie(NULL);                    // untie std::cin && std::cout
 
-  i = 0;
-  while (i < number - 1) {
-    j = 1;
-    while (j < number - i) {
-      if (board[i] != -1 && board[i + j] != -1) {
-        if (board[i] == board[i + j] + j)
-          return (0);
-        if (board[i] + j == board[i + j])
-          return (0);
-      }
-      j++;
-    }
-    i++;
-  }
-  return (1);
-}
+    int n;
+    int board[16][16];
 
-void solve(int *board, int solved, int *count, int number) {
-  int i;
+    cin >> n;
 
-  i = 0;
-  if (solved == number) {
-    *count = *count + 1;
-  } else {
-    while (i < number) {
-      board[solved] = i;
-      if (is_valid_row(board, number) == 1 &&
-          is_valid_diagonal(board, number) == 1)
-        solve(board, solved + 1, count, number);
-      board[solved] = -1;
-      i++;
-    }
-  }
-}
+    for (int i = 0; i < 16; i++)
+        for (int j = 0; j < 16; j++)
+            board[i][j] = 0;
 
-int ten_queens_puzzle(int number) {
-  int *board;
-  int count;
-  int i;
-
-  board = (int *)malloc(sizeof(int) * number);
-  i = 0;
-  count = 0;
-  while (i < number) {
-    board[i] = -1;
-    i++;
-  }
-  solve(board, 0, &count, number);
-  free(board);
-  board = 0;
-  return (count);
-}
-
-int main(void) {
-  int answer;
-
-  answer = 0;
-  scanf("%d", &answer);
-  printf("%d\n", ten_queens_puzzle(answer));
-  return (0);
+    cout << nqueen(1, n, board);
+    return 0;
 }
